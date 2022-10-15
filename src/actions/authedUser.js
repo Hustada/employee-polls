f3faf470ead4aa66d8ca1083a008682c72acb73a
hustada <mark.hustad@protonmail.com>
2022-10-15 08:35:06 -0500

    sub user for id

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,10 +1,10 @@
 export const SET_AUTHED_USER = "SET_AUTHED_USER";
 export const LOGOUT = "LOGOUT";
 
-export function setAuthedUser(id) {
+export function setAuthedUser(user) {
   return {
     type: SET_AUTHED_USER,
-    id,
+    user,
   };
 }
 
