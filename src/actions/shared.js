e164da6f3385bfd7fc18e03e9221c283fb58612a
hustada <mark.hustad@protonmail.com>
2022-10-23 14:19:23 -0500

    meh

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -17,6 +17,7 @@ export function handleInitialData() {
 
 export function handleAddQuestion(question) {
   return (dispatch, getState) => {
+    const { authedUser } = getState();
 
     dispatch(showLoading());
     return saveQuestion(question)
@@ -24,7 +25,7 @@ export function handleAddQuestion(question) {
           dispatch(addQuestion(question));
           dispatch(addQuestionToUser({
               qid: question.id,
-              author: question.author,
+              author: authedUser,
           }));
       })
       .then(() => dispatch(hideLoading()));;
