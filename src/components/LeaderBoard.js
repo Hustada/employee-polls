147ba4a7b4a04465535d2ab742edd267cc7e93c8
hustada <mark.hustad@protonmail.com>
2022-10-31 13:19:08 -0500

    leaderboard UI, question votes and percentages work minus dumb NaN on new questions(fix later)

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,5 +1,25 @@
 import React from 'react'
 import { connect } from "react-redux";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Container } from '@mui/material';
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 const LeaderBoard = (users) => {
 
@@ -13,10 +33,36 @@ const LeaderBoard = (users) => {
     }))
     .sort((a, b) => b.answered + b.created - (a.answered + a.created));
 
+    console.log(users);
+
   return (
-    <div>
-      { console.log(orderedUsers) }
-    </div>
+    <Container sx={{ mt: 5 }}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Users</TableCell>
+              <TableCell align="right">Answered</TableCell>
+              <TableCell align="right">Created</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   )
 }
 
--- src/components/ShowQuestion.js
+++ src/components/ShowQuestion.js
@@ -28,52 +28,34 @@ const withRouter = (Component) => {
   return ComponentWithRouterProp;
 };
 
-
 const ShowQuestion = ({ dispatch, questions, users, authedUser, id }) => {
-  const [optionOneVote, setOptionOneVote] = useState("");
-  const [optionTwoVote, setOptionTwoVote] = useState("");
-  const [disabled, setDisabled] = useState(false);
+
+  function formatAsPercent(num) {
+    return new Intl.NumberFormat('default', {
+      style: 'percent',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    }).format(num / 100);
+  }
+
   const question = questions[id];
   const user = users[authedUser];
   const avatar = question ? users[question.author].avatarURL : "";
   const optionOneVotes = question.optionOne.votes.length;
   const optionTwoVotes = question.optionTwo.votes.length;
   const totalVotes = optionOneVotes + optionTwoVotes;
-  const optionOnePercentage = optionOneVotes / totalVotes;
-  console.log('OPTION ONE VOTES:', optionOneVotes)
-  console.log('OPTION TWO VOTES:', optionTwoVotes)
-  console.log('TOTAL VOTES:', totalVotes);
-  console.log('OPTION ONE PERCENTAGE:', optionOnePercentage);
+  const optionOnePercentage = formatAsPercent((optionOneVotes / totalVotes) * 100);
+  const optionTwoPercentage = formatAsPercent((optionTwoVotes / totalVotes) * 100);
 
-  const handleOptionOneVote = (e) => {
-    console.log(selectedAnswer);
+  const handleVotes = (e) => {
     e.preventDefault();
-    setOptionOneVote("optionOne");
-    setOptionTwoVote("");
+
+    const selectedAnswer ={
+      authedUser: authedUser.id,
+      qid: id,
+      answer: e.currentTarget.value,
+    }
     dispatch(handleQuestionAnswer(selectedAnswer))
-  }
-
-  const handleOptionTwoVote = (e) => {
-    e.preventDefault();
-    console.log(selectedAnswer);
-    setOptionTwoVote("optionTwo");
-    setOptionOneVote("");
-    dispatch(handleQuestionAnswer(selectedAnswer))
-
-  }
-
-  const selectedOoptions = {
-    optionOneVote,
-    optionTwoVote,
-  }
-
-  console.log(optionOneVote);
-  console.log(optionTwoVote);
-
-  const selectedAnswer ={
-    authedUser: authedUser.id,
-    qid: id,
-    answer: "optionTwo", // fix this.
   }
 
   return (
@@ -90,13 +72,14 @@ const ShowQuestion = ({ dispatch, questions, users, authedUser, id }) => {
         src={avatar}
         sx={{ width: 300, height: 300, alignItems: "center", m: 3}}
       />
-      <Typography variant='h5'>Would You Rather?</Typography>
+      <Typography variant='h4' sx={{ mb: 5 }}>{totalVotes} Total Votes</Typography>
+      <Typography variant='h3'>Would You Rather?</Typography>
     </Box>
-    <Grid container spacing={3}>
+    <Grid container spacing={5} sx={{ mb: 10 }}>
       <Grid item xs={12} sm={6} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <Typography variant="h5">{optionOneVotes} Total Votes</Typography>
+        <Typography variant="h5" sx={{mb: 2}}>{optionOneVotes} Votes</Typography>
       <Box sx={{
-          borderColor: '1 px solid gray',
+          borderColor: '1px solid gray',
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
@@ -118,14 +101,16 @@ const ShowQuestion = ({ dispatch, questions, users, authedUser, id }) => {
           sx={{ backgroundColor: "orange" }}
           variant="contained"
           fullWidth
-          onClick={handleOptionOneVote}
+          onClick={handleVotes}
+          value="optionOne"
           // disabled={optionTwoVote === true}
         >
           Vote
         </Button>
+        <Typography variant="h5" sx={{ mt: 2 }}>Percent Voted: { isNaN(optionOnePercentage) ? optionOnePercentage : 0 }</Typography>
       </Grid>
       <Grid item xs={12} sm={6} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-        <Typography variant="h5">{optionTwoVotes} Total Votes</Typography>
+        <Typography variant="h5" sx={{mb: 2}} >{optionTwoVotes} Votes</Typography>
         <Box sx={{
           borderColor: '1 px solid gray',
           display: 'flex',
@@ -149,11 +134,13 @@ const ShowQuestion = ({ dispatch, questions, users, authedUser, id }) => {
           sx={{ backgroundColor: "orange" }}
           variant="contained"
           fullWidth
-          onClick={handleOptionTwoVote}
+          onClick={handleVotes}
+          value="optionTwo"
           // disabled={optionOneVote === true}
         >
           Vote
         </Button>
+        <Typography variant="h5" sx={{ mt: 2 }}>Percent Voted: { isNaN(optionTwoPercentage) ? optionTwoPercentage : 0 }</Typography>
       </Grid>
       <Grid item xs={6}>
         
